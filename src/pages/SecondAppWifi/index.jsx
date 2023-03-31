9c4bc42c4d63dc66d87e2a1020f11ed1efc00705
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-04-01 04:56:41 +0530

    Add simple heartbeat mechanism

--- src/pages/SecondAppWifi/index.jsx
+++ src/pages/SecondAppWifi/index.jsx
@@ -1,18 +1,14 @@
 import React, { useState } from "react";
 import { View, Text, Button } from "react-native";
 import WifiManager from "react-native-wifi-reborn";
-import { useDispatch, useSelector } from "react-redux";
-import { addCount } from "../../store/reducers/counter";
-import { countSelector } from "../../store/selectors/counter";
-import PermissionsComponent from "./PermissionsComponent";
 
 export default function SecondAppWiFi() {
+  console.log(new Date().toLocaleTimeString(), SecondAppWiFi.name);
+
   const [wifiInfo, setWifiInfo] = useState("");
   const [hotspotInfo, setHotspotInfo] = useState("");
-  const [_count, setCount] = useState(0);
 
   const getCurrentWifiInfo = async () => {
-    setCount((_) => (_ += 1));
     try {
       const ssid = await WifiManager.getCurrentWifiSSID();
       setWifiInfo(`Current WiFi SSID: ${ssid}`);
@@ -32,7 +28,7 @@ export default function SecondAppWiFi() {
 
   return (
     <View>
-      <Text>{wifiInfo || "Nothing here123" + _count}</Text>
+      <Text>{wifiInfo || "Nothing here"}</Text>
       <Button onPress={getCurrentWifiInfo} title="Get Current WiFi Info" />
       <Text>{hotspotInfo || "Nothing here"}</Text>
       <Button onPress={getHotspotInfo} title="Get Hotspot Info" />
