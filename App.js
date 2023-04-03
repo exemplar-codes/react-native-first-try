ae36a58bed9484f357e38f53d8ab6d70e3fd2b03
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-04-04 02:11:35 +0530

    position: static works in react-native, but only when target is web

--- App.js
+++ App.js
@@ -65,44 +65,45 @@ const App: () => Node = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  return (
-    <Provider store={store}>
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? "light-content" : "dark-content"}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}
-        >
-          <Header />
-          {/* <FirstApp /> */}
-          <SecondAppWiFi />
-          <View
-            style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            }}
-          >
-            <Section title="Step One">
-              Edit <Text style={styles.highlight}>App.js</Text> to change this
-              screen and then come back to see your edits.
-            </Section>
-            <Section title="See Your Changes">
-              <ReloadInstructions />
-            </Section>
-            <Section title="Debug">
-              <DebugInstructions />
-            </Section>
-            <Section title="Learn More">
-              Read the docs to discover what to do next:
-            </Section>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Provider>
-  );
+  // return (
+  //   <Provider store={store}>
+  //     <SafeAreaView style={backgroundStyle}>
+  //       <StatusBar
+  //         barStyle={isDarkMode ? "light-content" : "dark-content"}
+  //         backgroundColor={backgroundStyle.backgroundColor}
+  //       />
+  //       <ScrollView
+  //         contentInsetAdjustmentBehavior="automatic"
+  //         style={backgroundStyle}
+  //       >
+  //         <Header />
+  //         {/* <FirstApp /> */}
+  //         <SecondAppWiFi />
+  //         <View
+  //           style={{
+  //             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+  //           }}
+  //         >
+  //           <Section title="Step One">
+  //             Edit <Text style={styles.highlight}>App.js</Text> to change this
+  //             screen and then come back to see your edits.
+  //           </Section>
+  //           <Section title="See Your Changes">
+  //             <ReloadInstructions />
+  //           </Section>
+  //           <Section title="Debug">
+  //             <DebugInstructions />
+  //           </Section>
+  //           <Section title="Learn More">
+  //             Read the docs to discover what to do next:
+  //           </Section>
+  //           <LearnMoreLinks />
+  //         </View>
+  //       </ScrollView>
+  //     </SafeAreaView>
+  //   </Provider>
+  // );
+  return <FirstApp />;
 };
 
 const styles = StyleSheet.create({
--- src/pages/FirstApp.jsx
+++ src/pages/FirstApp.jsx
@@ -1,13 +1,57 @@
-import Counter from "../components/Counter"; // useState works
-import GitHubGet from "../components/GitHubGet"; // fetch, useEffect works.
-import ReduxStoreState from "../components/ReduxStoreState"; // Redux (RTK) works without issues
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 
-export default function FirstApp() {
-  return (
-    <>
-      <ReduxStoreState />
-      <Counter />
-      <GitHubGet />
-    </>
-  );
+export default class FlexDirectionBasics extends Component {
+  render() {
+    return (
+      // Try setting `flexDirection` to 'column'/'column-reverse'/'row'/'row-reverse'
+      <View style={styles.container}>
+        <Text style={styles.headerStyle}>flexDirection: 'row'</Text>
+
+
+        <LBox {...{}}>
+          <MBox {...{right: 0, bottom: 0, backgroundColor: 'yellow', position: 'static'}}>
+            <SBox {...{ position: 'absolute', right: 0, top: 0 }} />
+          </MBox>
+        </LBox>
+
+
+      </View>
+    );
+  }
+}
+
+function LBox(props = {}) {
+  return <View style={[{ flexDirection: 'row' }, styles.elementsContainer]}>
+    <View style={{ width: 200, height: 200, backgroundColor: '#EE2C38', ...props }} >
+      {props.children}
+    </View>
+  </View>
+}
+function MBox(props = {}) {
+  return <View style={{ width: 100, height: 100, backgroundColor: '#FAA030', ...props }} >
+    {props.children}
+  </View>
+}
+function SBox(props = {}) {
+  return <View style={{ width: 50, height: 50, backgroundColor: '#32B76C', ...props }} />
+}
+const styles = {
+  container: {
+    marginTop: 48,
+    flex: 1
+  },
+  headerStyle: {
+    fontSize: 24,
+    textAlign: 'center',
+    fontWeight: '100',
+    marginBottom: 24
+  },
+  elementsContainer: {
+    flex: 1,
+    backgroundColor: '#ecf5fd',
+    marginLeft: 24,
+    marginRight: 24,
+    marginBottom: 24
+  }
 }
